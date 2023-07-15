cee82f24a427b3692cd48645d359543b27ba9bf8
Kevin Glavin <kglavin@gmail.com>
2023-07-14 19:13:37 -0700

    3 freqs in parallel

--- projects/00_Audio_Signals/00_sine_gen.py
+++ projects/00_Audio_Signals/00_sine_gen.py
@@ -1,6 +1,7 @@
 import math
 import wave
 import pyaudio
+import struct
 
 # Audio settings
 sample_rate = 44100  # Number of samples per second
@@ -8,7 +9,7 @@ duration = 5  # Duration in seconds
 
 # Frequencies
 #frequencies = [100, 300, 900, 2200]
-frequencies = [ 900]
+frequencies = [ 300, 900, 2200]
 
 # Calculate the number of frames
 num_frames = int(sample_rate * duration)
@@ -16,7 +17,8 @@ num_frames = int(sample_rate * duration)
 # Generate the mixed sine wave data
 data = []
 for i in range(num_frames):
-    sample = sum(math.sin(2 * math.pi * freq * i / sample_rate) for freq in frequencies)
+    # add the samples but divide by number of freqs to keep value in range ( short )
+    sample = sum(math.sin(2 * math.pi * freq * i / sample_rate) for freq in frequencies)/len(frequencies)
     data.append(sample)
 
 # Configure the wave file
@@ -27,7 +29,7 @@ wave_file.setframerate(sample_rate)  # Sample rate
 wave_file.setcomptype("NONE", "Uncompressed")
 
 # Write the audio data to the file
-wave_file.writeframes(b"".join([wave_file.struct.pack("<h", int(sample * 32767)) for sample in data]))
+wave_file.writeframes(b"".join([struct.pack("<h", int(sample * 32767)) for sample in data]))
 wave_file.close()
 
 # Play the generated audio sample
@@ -38,7 +40,7 @@ stream = p.open(format=p.get_format_from_width(wave_file.getsampwidth()),
                 rate=wave_file.getframerate(),
                 output=True)
 
-wave_file.open("mixed_sine_wave.wav", "rb")
+wave_file = wave.open("mixed_sine_wave.wav", "rb")
 data = wave_file.readframes(num_frames)
 
 stream.write(data)
